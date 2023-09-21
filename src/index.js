5845f44ff28301ff9b2760473e685341b6fde6bb
Gennarro@yandex.ru <Gennarro@yandex.ru>
2023-09-21 18:46:43 +0300

    init:commit for complited task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    let nn = String(n).split('').reduce((num,n)=>{
+        num.unshift(n)
+        return num
+    },[])
+  return nn.join('')
 }
